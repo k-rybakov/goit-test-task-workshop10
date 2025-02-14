b080c61ac015e6ce04c57857aae31245cbef3f04
Konstantin Rybakov <konstantin.rybakov@nmi.com>
2025-02-14 12:15:09 +0200

    format text in tag

--- src/components/TagOption/TagOption.jsx
+++ src/components/TagOption/TagOption.jsx
@@ -1,4 +1,5 @@
 import styles from "./TagOption.module.css";
+import { capitalize } from "../../heplers/formatters";
 
 const TagOption = ({ icon, text, iconClass = "icon" }) => {
   return (
@@ -6,7 +7,7 @@ const TagOption = ({ icon, text, iconClass = "icon" }) => {
       <svg className={styles[iconClass]} aria-hidden="true">
         <use href={`/sprite.svg?ver=2#${icon}`} />
       </svg>
-      <span className={styles.text}>{text}</span>
+      <span className={styles.text}>{capitalize(text)}</span>
     </div>
   );
 };
